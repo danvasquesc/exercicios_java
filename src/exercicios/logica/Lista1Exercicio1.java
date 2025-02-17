ae89c8389ee70618e01f9a2eb6c8388b6e00637f
Dan Vasques <dan.vasques@outlook.com.br>
2025-02-17 14:07:25 -0300

    primeiro commit do projeto

---
+++ src/exercicios/logica/Lista1Exercicio1.java
@@ -0,0 +1,29 @@
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.*/
+
+package exercicios.logica;
+
+import java.util.Scanner;
+
+public class Lista1Exercicio1 {
+
+	public static void main(String[] args) {
+
+		int valor1, valor2, soma;
+
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Digite o primeiro valor inteiro: ");
+			valor1 = sc.nextInt();
+
+			System.out.print("Digite o segundo valor inteiro: ");
+			valor2 = sc.nextInt();
+
+			soma = valor1 + valor2;
+
+			System.out.print("A soma é = " + soma);
+
+		}
+
+	}
+
+}
