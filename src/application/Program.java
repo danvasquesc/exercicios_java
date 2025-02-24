1c7c15bd30f552037cf798ab3ae7096683c5a726
Dan Vasques <dan.vasques@outlook.com.br>
2025-02-24 14:17:34 -0300

    criado as entradas e o objeto product

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			Product product = new Product(); /*declaracao de uma variavel do tipo Product*/
+			
+			System.out.println("Enter product data: ");
+			System.out.print("Name: ");
+			product.name = sc.nextLine(); /*o 'name' ja foi declarado dentro da classe Product, e agora o que o usuario digitar pro 'name' vai ser alocado dentro do objeto 'product' em especifico no 'name'*/
+			
+			System.out.print("Price: ");
+			product.price = sc.nextDouble();
+			
+			System.out.print("Quantity in stock: ");
+			product.quantity = sc.nextInt();
 
+			
+			
+			
+		}
+		
 	}
-
+	
 }
