55466adbda46a4aad6ab4124ac9a03e1a2ea08e6
Dan Vasques <dan.vasques@outlook.com.br>
2025-02-24 14:48:53 -0300

    criado metodo toString

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,28 +8,31 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
-		
+
 		try (Scanner sc = new Scanner(System.in)) {
-			
-			Product product = new Product(); /*declaracao de uma variavel do tipo Product*/
-			
+
+			Product product = new Product(); /* declaracao de uma variavel do tipo Product */
+
 			System.out.println("Enter product data: ");
 			System.out.print("Name: ");
-			product.name = sc.nextLine(); /*o 'name' ja foi declarado dentro da classe Product, e agora o que o usuario digitar pro 'name' vai ser alocado dentro do objeto 'product' em especifico no 'name'*/
-			
+			product.name = sc.nextLine(); /*
+											 * o 'name' ja foi declarado dentro da classe Product, e agora o que o
+											 * usuario digitar pro 'name' vai ser alocado dentro do objeto 'product' em
+											 * especifico no 'name'
+											 */
+
 			System.out.print("Price: ");
 			product.price = sc.nextDouble();
-			
+
 			System.out.print("Quantity in stock: ");
 			product.quantity = sc.nextInt();
+			
+			System.out.println(product);
 
-			
-			
-			
 		}
-		
+
 	}
-	
+
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,23 +1,51 @@
 package entities; /*o produto é uma entidade*/
 
 public class Product {
-	
-	public String name; /*atributo com letra minuscula*/
+
+	public String name; /* atributo com letra minuscula */
 	public double price;
 	public int quantity;
-	
-	/*metodo calcula o preço baseado na quantidade, para ter o valor final*/
-	public double totalValueInStock() { 
+
+	/* metodo calcula o preço baseado na quantidade, para ter o valor final */
+	public double totalValueInStock() {
 		return price * quantity;
 	}
-	
-	/*metodo vai pegar o valor alocado no atributo 'quantity' e somar com o valor que o usuario vai passar atraves do parametro 'quantity'. esse 'quantity' dentro do parametro é um argumento que o usuario vai digitar*/
+
+	/*
+	 * metodo vai pegar o valor alocado no atributo 'quantity' e somar com o valor
+	 * que o usuario vai passar atraves do parametro 'quantity'. esse 'quantity'
+	 * dentro do parametro é um argumento que o usuario vai digitar
+	 */
 	public void addProducts(int quantity) {
-		this.quantity += quantity; /*this para referenciar o atributo 'quantity' e somar com o parametro 'quantity'*/
+		this.quantity += quantity; /*
+									 * this para referenciar o atributo 'quantity' e somar com o parametro
+									 * 'quantity'
+									 */
 	}
-	
-	/*metodo remove a quantidade de produtos*/
+
+	/* metodo remove a quantidade de produtos */
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
+
+	/* metodo para sobrepor o metodo padrao 'toString' */
+	/*
+	 * public String toString() { 
+	 * return name; /* no Program eu posso imprimir
+	 * product.toString, chamando esse metodo, e ai vai aparecer na tela o dado
+	 * alocado na variavel 'name'. também nao precisa colocar o metodo toString, se
+	 * colocar apenas pra imprimir 'product', depois de ter criado esse metodo, vai
+	 * aparecer o 'name', pois o java reconhece automaticamente
+	 */
+
+	/*metodo oficial pra saida de dados do Program*/
+	public String toString() {
+		return name + ", $ " 
+					+ String.format("%.2f", price) /*formatar a saida para ter dois zeros decimais, do que um*/
+					+ ", " 
+					+ quantity 
+					+ " units, Total: $ " 
+					+ String.format("%.2f", totalValueInStock());
+	}
+	
 }
