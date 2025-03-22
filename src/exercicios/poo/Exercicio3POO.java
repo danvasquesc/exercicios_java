77b9cc336a7941c2524f33722ad30c58135ab6e9
Dan Vasques <dan.vasques@outlook.com.br>
2025-03-22 16:20:04 -0300

    programa calcula nota de aluno, verifica se passou ou nao e pontos faltantes

---
+++ src/exercicios/poo/Exercicio3POO.java
@@ -0,0 +1,40 @@
+/*Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+ano. Dizer também se o aluno está aprovado (PASS) ou não (FAILED) e, em caso negativo, quantos pontos faltam
+para o aluno obter o mínimo para ser aprovado (que é 60% da nota). Você deve criar uma classe Student para
+resolver este problema.*/
+
+package exercicios.poo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio3POO {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		try (Scanner sc = new Scanner(System.in)) {
+
+			Student aluno = new Student();
+
+			aluno.name = sc.nextLine();
+			aluno.grade1 = sc.nextDouble();
+			aluno.grade2 = sc.nextDouble();
+			aluno.grade3 = sc.nextDouble();
+
+			System.out.printf("FINAL GRADE = %.2f%n", aluno.finalGrade());
+
+			if (aluno.finalGrade() < 60) {
+				System.out.println("FAILED");
+				System.out.printf("MISSING %.2f POINTS%n", aluno.missingPoints());
+			} else {
+				System.out.println("PASS");
+			}
+
+		}
+
+	}
+
+}
---
+++ src/exercicios/poo/Student.java
@@ -0,0 +1,26 @@
+/*esta classe pertence a Exercicio3POO*/
+
+package exercicios.poo;
+
+public class Student {
+
+	public String name;
+	public double grade1;
+	public double grade2;
+	public double grade3;
+
+	public double finalGrade() {
+		return grade1 + grade2 + grade3;
+	}
+
+	public double missingPoints() {
+
+		if (finalGrade() < 60.0) {
+			return 60.0 - finalGrade();
+		} else {
+			return 0.0; /* Aqui retorna 0 pq nao retornará nenhum numero, apenas 'PASS" */
+		}
+
+	}
+
+}
