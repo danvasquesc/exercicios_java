b1296d7cf68b8a47663d79256ffdbb007adb6b53
Dan Vasques <dan.vasques@outlook.com.br>
2025-03-24 13:13:21 -0300

    versao2 - instanciando objetos - aula de membros estaticos

---
+++ src/exercicios/poo/CalculatorV2.java
@@ -0,0 +1,18 @@
+/*Essa classe pertence a classe 'Versao2', e poderia ser criada no pacote 'util' pois é uma classe utilitária*/
+
+package exercicios.poo;
+
+public class CalculatorV2 {
+	
+	/*retirou o 'static' pois agora pode*/
+	public final double PI = 3.14159;
+		
+	public double circumference(double radius) {
+		return 2.0 * PI * radius; 
+	}
+	
+	public double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+	}
+
+}
---
+++ src/exercicios/poo/Versao2.java
@@ -0,0 +1,33 @@
+/*Classe criada para estudar Membros Estáticos - Parte 1*/
+
+package exercicios.poo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Versao2 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			CalculatorV2 calc = new CalculatorV2(); /*instanciar objeto*/
+
+			System.out.print("Enter radius: ");
+			double radius = sc.nextDouble();
+
+			double c = calc.circumference(radius);
+
+			double v = calc.volume(radius);
+
+			System.out.printf("Circumference: %.2f%n", c);
+			System.out.printf("volume: %.2f%n", v);
+			System.out.printf("PI value: %.2f%n", calc.PI);
+
+		}
+
+	}
+
+}
