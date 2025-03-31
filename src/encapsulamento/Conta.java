0cea9fe874dd8ecffd8714066dbabcede2af35f7
Dan Vasques <dan.vasques@outlook.com.br>
2025-03-31 11:31:52 -0300

    programa revisado e corrigido com as instrucoes do professor

--- src/encapsulamento/Conta.java
+++ src/encapsulamento/Conta.java
@@ -8,22 +8,42 @@ public class Conta {
 	private String nome;
 	double saldo;
 
-	public Conta(int numero, String nome, double saldo) {
-		this.numero = numero;
-		this.nome = nome;
-		this.saldo = saldo;
-	}
+	/*
+	 * como o exercicio o usuario pode informar tres argumentos ou dois (pode nao
+	 * ter o deposito inicial, criamos dois construtores, um com os tres atributos e
+	 * outro sem o atributo saldo
+	 */
 
 	/*
-	 * Sobrecarga - sem o parametro 'valor', pois ele pode iniciar em 0, se nao
-	 * tiver deposito
+	 * Sem o parametro 'saldo', pois ele pode iniciar em 0, se nao tiver deposito/
+	 * aqui o usuario nao digitou o deposito inicial
 	 */
 	public Conta(int numero, String nome) {
 		this.numero = numero;
 		this.nome = nome;
 	}
 
-	/* GET e SET - o numero da conta nao recebe set pois nao pode ser alterado */
+	/*
+	 * Sobrecarga - no parametro trocamos o nome de 'saldo' para 'depositoInicial'
+	 * pra ficar mais facil de entender que esse construtor serve pro deposito
+	 * inicial, não o saldo, pois saldo vai ter em todas as situações. O valor que o
+	 * usuario digita no inicio é o deposito inicial
+	 */
+	public Conta(int numero, String nome, double depositoInicial) {
+		this.numero = numero;
+		this.nome = nome;
+		deposito(depositoInicial); /*
+									 * aqui é chamado o metodo 'deposito' que recebe como parametro o
+									 * 'depositoInicial'. Esta operacao facilita caso a regra de negocio mude, por
+									 * exemplo: caso o banco queira adicionar uma taxa no desposito, nao é
+									 * necessario alterar o construtor, somente o metodo de deposito
+									 */
+	}
+
+	/*
+	 * GET e SET o numero da conta nao recebe SET pois nao pode ser alterado o saldo
+	 * só pode ser alterado com metodos especificos, entao não é criado o metodo SET
+	 */
 	public int getNumero() {
 		return numero;
 	}
@@ -40,27 +60,28 @@ public class Conta {
 		return saldo;
 	}
 
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
+	/*
+	 * metodos para deposito e saque cria-se o parametro 'quantia' para nao
+	 * confundir com o saldo esse parametro pertence a esses metodos
+	 */
+
+	public void deposito(double quantia) {
+		saldo = saldo + quantia; /*
+									 * aqui retirei o 'tihs' pq temos o parametro 'quantia', entao nao precisa
+									 * referenciar o 'saldo'
+									 */
+
 	}
 
-	/* metodos para deposito e saque */
-
-	public void deposito(double saldo) {
-		this.saldo += saldo;
-
-	}
-
-	public void saque(double saldo) {
-		this.saldo = this.saldo - (saldo + 5.0); /*
-													 * this.saque era 700, ai adicionei a taxa ao valor que quero sacar
-													 * e o resultado subtraio do this.saque
-													 */
+	public void saque(double quantia) {
+		saldo = saldo - (quantia + 5.0); /*
+											 * o saldo era 700, ai adicionei a taxa ao valor que quero sacar que é a
+											 * 'quantia' e o resultado subtraio do saldo
+											 */
 	}
 
 	public String toString() {
 		return "Account " + numero + ", Holder: " + nome + String.format(", Balance: $ %.2f", saldo);
 	}
 
-	
 }
--- src/encapsulamento/ExercicioContaBancaria.java
+++ src/encapsulamento/ExercicioContaBancaria.java
@@ -10,39 +10,52 @@ public class ExercicioContaBancaria {
 
 		try (Scanner sc = new Scanner(System.in)) {
 
+			Conta conta; /*
+							 * criado variavel do objeto para poder instanciar ela no if. Se eu instanciar
+							 * no if, o processo fica limitado só dentro da estrutura, não fora
+							 */
+
 			System.out.print("Enter account number: ");
 			int numero = sc.nextInt();
 
 			System.out.print("Enter account holder: ");
-			String nome = sc.next();
+			sc.nextLine(); /*
+							 * serve para limpar o buffer e permitir ler o nome que possue espaço, por
+							 * exemplo 'Alex Green'
+							 */
+			String nome = sc.nextLine();
 
 			System.out.print("Is there na initial deposit (y/n)? ");
 			char confirmacaoDeposito = sc.next().charAt(0);
 
-			double saldo = 0;
-
 			if (confirmacaoDeposito == 'y') {
 				System.out.print("Enter initial deposit value: ");
-				saldo = sc.nextDouble();
+				double depositoInicial = sc.nextDouble();
+				conta = new Conta(numero, nome,
+						depositoInicial); /*
+											 * aqui instanciei o construtor com os tres parametros, pois vai ter
+											 * deposito inicial - Aqui é o construtor de Sobrecarga
+											 */
+			} else {
+				conta = new Conta(numero, nome); /* aqui cria a conta com o construtor de dois argumentos */
 			}
-
-			Conta conta = new Conta(numero, nome, saldo);
 
 			System.out.println();
 			System.out.println("Account data: \n" + conta);
 
 			System.out.println();
 			System.out.print("Enter a deposit value: ");
-			saldo = sc.nextDouble();
-			conta.deposito(saldo);
-
+			double valorDeposito = sc.nextDouble();
+			conta.deposito(valorDeposito); /*
+											 * o metodo 'deposito' vai receber essa variavel 'valorDeposito' para fazer
+											 * sua acao
+											 */
 			System.out.println("Update account data: \n" + conta);
 
 			System.out.println();
 			System.out.print("Enter a withdraw value: ");
-			saldo = sc.nextDouble();
-			conta.saque(saldo);
-			
+			double valorSaque = sc.nextDouble();
+			conta.saque(valorSaque);
 			System.out.println("Update account data: \n" + conta);
 
 		}
