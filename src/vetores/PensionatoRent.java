d220e2741cb860ed75f14fc3d6dc51805c411133
Dan Vasques <dan.vasques@outlook.com.br>
2025-04-16 17:01:30 -0300

    resolvendo conflitos de pull

--- src/vetores/PensionatoRent.java
+++ src/vetores/PensionatoRent.java
@@ -1,31 +1,26 @@
 package vetores;
 
 public class PensionatoRent {
-	
+
 	private String name;
 	private String email;
-	
-	
+
 	public PensionatoRent(String name, String email) {
 		this.name = name;
 		this.email = email;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
@@ -34,16 +29,5 @@ public class PensionatoRent {
 	public String toString() {
 		return name + ", " + email;
 	}
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
